df3ad780810adc6a86859e65304d574d0c95bc01
Espi Marisa <contact@espi.me>
2024-09-23 12:00:14 -0500

    chore: add initial stuff

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# media-stack
-My personal stack of self-hosted media tools.
+# espinet
+
+My homelab setup and configs.
+
+## Setup
+
+Copy `.env.example` to `.env` and add your environment variables. Be sure to review `docker-compose.yml` for any options you need to change.
+
+## Updating
+
+Run `scripts/update.sh`. If you have any local changes to `docker-compose.yml`, they will be overwritten. It's recommended to fork this repository to keep your base config up to date more easily if you wish to use this script.
+
+## License
+
+[zlib/libpng](LICENSE.md)
---
+++ scripts/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Creates volumes
+docker volume create postgresql-volume
+docker volume create redis-volume
+docker volume create nginx-volume
+docker volume create nextcloud-volume
+docker volume create searxng-volume
+docker volume create homepage-volume
+docker volume create qbittorrentvpn-volume
+docker volume create radarr-volume
+docker volume create sonarr-volume
+docker volume create prowlarr-volume
+docker volume create readarr-volume
+docker volume create jellyfin-volume
+
+# Creates network
+docker network create global-network
+docker network create download-network
